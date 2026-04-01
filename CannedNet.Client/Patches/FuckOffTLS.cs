6c9f4499e421c8efc25b4e1d8b0c4aee4a1879a9
Lapis <47952866+LapisGit@users.noreply.github.com>
2026-04-01 09:16:28 -0400

    add src

---
+++ CannedNet.Client/Patches/FuckOffTLS.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using Org.BouncyCastle.Crypto.Tls;
+
+namespace CannedNet.Client.Patches;
+
+public class FuckOffTLS
+{
+    [HarmonyPatch(typeof(LegacyTlsAuthentication), "NotifyServerCertificate")]
+    public class TlsPatch
+    {
+        private static bool Prefix()
+        {
+            return false;
+        }
+    }
+}
